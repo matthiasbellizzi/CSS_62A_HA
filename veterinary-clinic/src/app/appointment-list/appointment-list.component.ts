4a384ffd5b625377632dcbafdba5bd3d7ba2d79f
matthiasbellizzi <bellizzi.matthias@gmail.com>
2023-01-18 19:18:52 +0100

    EXport to excel and pdf

--- veterinary-clinic/src/app/appointment-list/appointment-list.component.ts
+++ veterinary-clinic/src/app/appointment-list/appointment-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Appointment } from './dto/appointment.dto';
 import { AppointmentBooking } from '../services/appointment.service';
+import * as XLSX from 'xlsx';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-appointment-list',
@@ -21,6 +23,8 @@ export class AppointmentListComponent implements OnInit {
 
   constructor(private appointmentBooking: AppointmentBooking) { }
 
+  @ViewChild('content', { static: false }) el!: ElementRef;
+  
   viewButton() {
     console.log('Detected a button click in VIEW');
   }
@@ -32,5 +36,26 @@ export class AppointmentListComponent implements OnInit {
   deleteButton() {
     console.log('Detected a button click in DELETE');
   }
+
   
+  exportexcel(): void
+  {
+    let element = document.getElementById('excel-table');
+    const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+ 
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+ 
+    XLSX.writeFile(wb, 'ExcelSheet.xlsx');
+  }
+
+  exportpdf(): void {
+    let pdf = new jsPDF()
+    pdf.setFontSize(24);
+    pdf.html(this.el.nativeElement, {
+      callback: (pdf) => {
+        pdf.save("PDFSheet.pdf")
+      }
+    })
+  }
 }
